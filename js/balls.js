191574db9388f531d045c6b2b8ae3505d7ca9f2b
tomaslaidlaw <97003193+tomaslaidlaw@users.noreply.github.com>
2022-01-14 16:03:24 -0300

    Update balls.js

--- js/balls.js
+++ js/balls.js
@@ -1,8 +1,8 @@
-<script>
+ 
 // Some random colors   const colors = ["#3CC157", "#2AA7FF", "#1B1B1B", "#FCBC0F", "#F85F36"];
 const colors = ["#ce4732", "#aedc3d", "#dcda3d", "#121212", "#222"];
 
-const numBalls = 100;
+const numBalls = 42;
 const balls = [];
 
 for (let i = 0; i < numBalls; i++) {
@@ -32,12 +32,11 @@ balls.forEach((el, i, ra) => {
       { transform: `translate(${to.x}rem, ${to.y}rem)` }
     ],
     {
-      duration: (Math.random() + 1) * 3000, // random duration
+      duration: (Math.random() + 1) * 6000, // random duration
       direction: "alternate",
       fill: "both",
       iterations: Infinity,
       easing: "ease-in-out"
     }
   );
-});
-</script>
+}); 
